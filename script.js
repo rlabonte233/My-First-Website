3b4b0439e0e312c2ba3fedb087c3f263ae42b302
rlabonte233 <122408236+rlabonte233@users.noreply.github.com>
2023-04-10 13:55:29 -0700

    Update index.html, script.js, main.css

--- script.js
+++
@@ -1,4 +0,0 @@
-var bodyHeight = document.body.clientHeight
-if(window.innerHeight>bodyHeight){
-    document.querySelector("footer").style = `position: absolute; left: 0; top: ${window.innerHeight - document.querySelector("footer").clientHeight}px:`
-}
